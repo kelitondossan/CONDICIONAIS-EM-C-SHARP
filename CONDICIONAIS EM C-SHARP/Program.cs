44634b7291f39b278e497b8c209b80e2735c5d59
keliton.santos <keliton.santos@ESTNTB003>
2022-02-14 09:16:39 -0300

    Adicionar arquivos de projeto.

---
+++ CONDICIONAIS EM C-SHARP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CONDICIONAIS_EM_C_SHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("DESCUBRA SE O NUMERO E PAR OU IMPAR\n");
+            Console.WriteLine("Entre com um numero inteiro");
+            int x = int.Parse(Console.ReadLine());
+
+            if ( x % 2 == 0) // essa condição if so para executar coisas verdadeiras 
+            {
+                Console.WriteLine("Par!");
+            }
+            else// else executa condiões falsas 
+            {
+                Console.WriteLine("impar");
+            }
+
+           
+
+
+        }
+    }
+}
---
+++ ESTRUTURA CONDICIONAL DE ENCADEAMENTO IF ELSE/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ESTRUTURA_CONDICIONAL_DE_ENCADEAMENTO_IF_ELSE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Qual a hora atual?");
+            int hora = int.Parse(Console.ReadLine());
+            if (hora < 12)
+            {
+                Console.WriteLine("Bom dia ");
+            }
+          else if ( hora < 18)
+            {
+                Console.WriteLine("Boa tarde ");
+            }
+            else
+            {
+                Console.WriteLine("Boa noite");
+            }
+        
+        }
+
+    }
+}
